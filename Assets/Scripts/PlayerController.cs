97268bb614a2a5831a4f5c81b64eb79a454d945e
Jordan Reese <jsreese92@gmail.com>
2015-07-21 20:30:39 -0400

    less shitty movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,35 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	
 	private Rigidbody rb;
+	private Vector3 movement; 
 	
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
 	}
-	
+
+	// for physics stuff
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
 		
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		// Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		
-		rb.AddForce (movement * speed);
+		// rb.AddForce (movement * speed);
+		Move (moveHorizontal, moveVertical);
 	}
+
+	void Move (float h, float v) 
+	{
+		movement.Set (h, 0f, v);
+
+		// magic incantation to normalize speed vector and
+		// normalize it to the tick speed 
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		// move the player
+		rb.MovePosition(transform.position + movement);
+	}
+
 }
